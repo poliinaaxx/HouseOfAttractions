b7bb487904a857b8103ad014b64855298d83dbb4
Polina <kirichenokpolina@gmail.com>
2024-04-21 20:26:36 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,90 @@
+// Questions
+function switchQuestions1(){
+    var btn1 = document.querySelector('#plus1');
+    var info1 = document.querySelector('#info1');
+    var qstn1 = document.querySelector('#qstn1');
+    btn1.classList.toggle('close');
+    info1.classList.toggle('answer-hide');
+    qstn1.classList.toggle('answer-open');
+}
+document.querySelector('#plus1').onclick = switchQuestions1;
+
+function switchQuestions2(){
+    var btn2 = document.querySelector('#plus2');
+    var info2 = document.querySelector('#info2');
+    var qstn2 = document.querySelector('#qstn2');
+    btn2.classList.toggle('close');
+    info2.classList.toggle('answer-hide');
+    qstn2.classList.toggle('answer-open');
+}
+document.querySelector('#plus2').onclick = switchQuestions2;
+
+function switchQuestions3(){
+    var btn3 = document.querySelector('#plus3');
+    var info3 = document.querySelector('#info3');
+    var qstn3 = document.querySelector('#qstn3');
+    btn3.classList.toggle('close');
+    info3.classList.toggle('answer-hide');
+    qstn3.classList.toggle('answer-open');
+}
+document.querySelector('#plus3').onclick = switchQuestions3;
+
+function switchQuestions4(){
+    var btn4 = document.querySelector('#plus4');
+    var info4 = document.querySelector('#info4');
+    var qstn4 = document.querySelector('#qstn4');
+    btn4.classList.toggle('close');
+    info4.classList.toggle('answer-hide');
+    qstn4.classList.toggle('answer-open');
+}
+document.querySelector('#plus4').onclick = switchQuestions4;
+
+function switchQuestions5(){
+    var btn5 = document.querySelector('#plus5');
+    var info5 = document.querySelector('#info5');
+    var qstn5 = document.querySelector('#qstn5');
+    btn5.classList.toggle('close');
+    info5.classList.toggle('answer-hide');
+    qstn5.classList.toggle('answer-open');
+}
+document.querySelector('#plus5').onclick = switchQuestions5;
+
+function switchQuestions6(){
+    var btn6 = document.querySelector('#plus6');
+    var info6 = document.querySelector('#info6');
+    var qstn6 = document.querySelector('#qstn6');
+    btn6.classList.toggle('close');
+    info6.classList.toggle('answer-hide');
+    qstn6.classList.toggle('answer-open');
+}
+document.querySelector('#plus6').onclick = switchQuestions6;
+
+function switchQuestions7(){
+    var btn7 = document.querySelector('#plus7');
+    var info7 = document.querySelector('#info7');
+    var qstn7 = document.querySelector('#qstn7');
+    btn7.classList.toggle('close');
+    info7.classList.toggle('answer-hide');
+    qstn7.classList.toggle('answer-open');
+}
+document.querySelector('#plus7').onclick = switchQuestions7;
+
+function switchQuestions8(){
+    var btn8 = document.querySelector('#plus8');
+    var info8 = document.querySelector('#info8');
+    var qstn8 = document.querySelector('#qstn8');
+    btn8.classList.toggle('close');
+    info8.classList.toggle('answer-hide');
+    qstn8.classList.toggle('answer-open');
+}
+document.querySelector('#plus8').onclick = switchQuestions8;
+
+function switchQuestions9(){
+    var btn9 = document.querySelector('#plus9');
+    var info9 = document.querySelector('#info9');
+    var qstn9 = document.querySelector('#qstn9');
+    btn9.classList.toggle('close');
+    info9.classList.toggle('answer-hide');
+    qstn9.classList.toggle('answer-open');
+}
+document.querySelector('#plus9').onclick = switchQuestions9;
